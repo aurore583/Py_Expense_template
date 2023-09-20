0923ad5bc7f176feab5609a436c4769ceb0bd97d
Aurore <aurore.bozec@epita.fr>
2023-09-20 21:27:55 +0200

    TD to choose spender

--- expense.py
+++ expense.py
@@ -1,4 +1,12 @@
+import csv
 from PyInquirer import prompt
+
+
+def choose_spender():
+    fichier = open("users.csv") #Ouvre le fichier des user
+    user_list = fichier.read() #lis le fichier
+    list_spender = user_list.split("\n") #selectionne chaque user sous forme de list
+    return list_spender
 
 expense_questions = [
     {
@@ -12,19 +20,22 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
-        "message":"New Expense - Spender: ",
+        "message":"New Expense - Spender (choose among this list:): ",
+        "choices": choose_spender(), 
     },
 
 ]
 
-
-
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    fichier = open("expense_report.csv", "a") #ouvrir un fichier pour append dedans
+    fichier.write(f'{infos["amount"]}, {infos["label"]}, {infos["spender"]}\n') 
     print("Expense Added !")
     return True
+
+
 
 
--- user.py
+++ user.py
@@ -1,8 +1,23 @@
+from shutil import copy2
 from PyInquirer import prompt
-user_questions = [
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"What is the name of the new user ? - Name: ",
+    },
 ]
 
 def add_user():
     # This function should create a new user, asking for its name
+    infos = prompt(user_questions)
+    fichier = open("users.csv", "a") #ouvrir un fichier pour append dedans
+    fichier.write(f'{infos["name"]}\n') 
+    print("User Added !")
+    return True
+
+
+def save_users(): #sauvegarder les users dans un autre fichier pour la redondance des infos
+    copy2("./users.csv", "./user_saved.csv")
     return
